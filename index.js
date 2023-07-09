9be77c0a34b241cf5670f8c9927a1617d1a5864d
Wayne Otido <otidowayne@gmail.com>
2023-07-10 00:01:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(plan = "roller-skate") {
+  return `This Saturday, I want to ${plan}!`;
+}
+saturdayFun();
+
+function mondayWork(check = "go to the office") {
+  return `This Monday, I will ${check}.`;
+}
+mondayWork("This Monday, I will work from home.");
+
+function wrapAdjective(adjective) {
+  const specialChar = function (start) {
+    return `You are ${adjective}${start}${adjective}!`;
+  }
+  return specialChar
+  
+}
+wrapAdjective("*")
